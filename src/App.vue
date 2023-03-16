c5e1d82aba2bc01e2254cf72e3ffb56faef25062
Yaël Busser <mrmaclevine@gmail.com>
2023-03-16 23:01:58 +0100

    ok

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <div class="flex flex-col h-screen">
+      <Navbar />
+
+      <div class="flex justify-center items-center flex-grow flex-col">
+        <router-view />
+      </div>
+    </div> 
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full px-6">
+                <div class="mt-8 relative rounded-lg bg-indigo-700 container mx-auto flex flex-col items-center pt-12 sm:pt-24 pb-24 sm:pb-32 md:pb-48 lg:pb-56 xl:pb-64">
+                   <img class="mr-2 lg:mr-12 mt-2 lg:mt-12 absolute right-0 top-0"  src="https://tuk-cdn.s3.amazonaws.com/can-uploader/center_aligned_with_image-svg2.svg" alt="bg"/>
+                   <img class="ml-2 lg:ml-12 mb-2 lg:mb-12 absolute bottom-0 left-0" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/center_aligned_with_image-svg3.svg" alt="bg"/>
+                   
+                    <div class="w-11/12 sm:w-2/3 mb-5 sm:mb-10">
+                        <h1 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-white font-bold leading-tight">The Freedom to Create the Pages You Want</h1>
+                    </div>
+                    <div class="flex justify-center items-center mb-10 sm:mb-20">
+                        <button class="hover:text-white hover:bg-transparent lg:text-xl hover:border-white border bg-white transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 ring-offset-indigo-700	focus:ring-white rounded text-indigo-700 px-4 sm:px-8 py-1 sm:py-3 text-sm">Get Started</button>
+                        <button class="hover:bg-white hover:text-indigo-600 lg:text-xl hover:border-indigo-600 ml-3 sm:ml-6 bg-transparent transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 ring-offset-indigo-700 focus:ring-white hover:bg-indigo-700-800 rounded border border-white text-white px-4 sm:px-8 py-1 sm:py-3 text-sm">Learn More</button>
+                    </div>
+                </div>
+                <div class="container mx-auto flex justify-center md:-mt-56 -mt-20 sm:-mt-40">
+                    <div class="relative sm:w-2/3 w-11/12">
+                        <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/hero/h_2.png" alt="Sample Page" role="img"  />
+                    </div>
+                </div>
+            </div>
+</template>
